37e454a41b80fd60bade9e05dc307a74d73cf7cf
MariaMaslovska <maria.maslovska124@gmail.com>
2020-04-03 22:06:44 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <p2>Home page</p2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente soluta perspiciatis debitis deserunt incidunt rerum id, molestiae laudantium quidem placeat!</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
